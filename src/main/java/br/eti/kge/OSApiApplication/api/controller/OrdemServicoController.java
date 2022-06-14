9cc21da41667ebb51759b1a705e1b988bd10a7e9
raelnascimento <nascimentoisrael19@gmail.com>
2022-06-14 14:25:51 -0300

    Adicionando metodos GET, PUT e Delete OSApiApplication - v1.0.1

--- src/main/java/br/eti/kge/OSApiApplication/api/controller/OrdemServicoController.java
+++ src/main/java/br/eti/kge/OSApiApplication/api/controller/OrdemServicoController.java
@@ -9,8 +9,10 @@ import br.eti.kge.OSApiApplication.domain.model.OrdemServico;
 import br.eti.kge.OSApiApplication.domain.repository.OrdemServicoRepository;
 import br.eti.kge.OSApiApplication.domain.service.OrdemServicoService;
 import java.util.List;
+import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -47,10 +49,12 @@ public class OrdemServicoController {
     
     @GetMapping("/{osID}")
     public OrdemServico listaID(@PathVariable long osID){
-        return ordemServicoRepository.findById(osID);
+        Optional <OrdemServico> ordemServico = ordemServicoRepository.findById(osID);
+        return ordemServico.orElse(null);
     } 
     @DeleteMapping("/{osID}")
     public OrdemServico deletar(@PathVariable long osID){
-        return ordemServicoRepository.findById(osID);
+        Optional <OrdemServico> ordemServico = ordemServicoRepository.findById(osID);
+        return ordemServico.orElse(null);
     } 
 }
